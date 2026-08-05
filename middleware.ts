a850a9a54c8704be1119649f053ec3980b7e0301
Aysha Jubapu <ayshajubapu20@gmail.com>
2026-08-05 21:26:05 +0530

    upsam

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { next } from '@vercel/edge';
+
+export default function middleware(request: Request) {
+  const url = new URL(request.url);
+
+  if (/^\/game\//i.test(url.pathname)) {
+    return new Response('Gone', { status: 410 });
+  }
+
+  return next();
+}
+
+export const config = {
+  matcher: ['/game/:path*'],
+};
